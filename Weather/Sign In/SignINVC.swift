117ac847a9cc3e0a0f3e99ef2fec7b81f93b7e9f
17hdfied <hardikwason@gmail.com>
2018-01-06 12:38:30 +0530

    login UI design

---
+++ Weather/Sign In/SignINVC.swift
@@ -0,0 +1,18 @@
+//
+//  SignINVC.swift
+//  Weather
+//
+//  Created by Hardik Wason on 06/01/18.
+//  Copyright © 2018 Hardik Wason. All rights reserved.
+//
+
+import UIKit
+
+class SignINVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
